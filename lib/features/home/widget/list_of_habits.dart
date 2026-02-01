8d4fc67d287c3f50402ace3e3aeeafad83fabd28
gloxinia13 <uzanzaev44@gmail.com>
2026-02-01 21:09:44 +0600

    minor changes

--- lib/features/home/widget/list_of_habits.dart
+++ lib/features/home/widget/list_of_habits.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:habit_tracker/models/habit.dart';
 
 class ListOfHabits extends StatelessWidget {
-  const ListOfHabits({
-    super.key,
-    required this.habits,
-  });
-
+  const ListOfHabits({super.key, required this.habits});
   final List<Habit> habits;
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double widgetHeight = screenHeight * 0.15;
+
     return ListView.separated(
       padding: EdgeInsets.all(14),
       itemCount: habits.length,
       itemBuilder: (BuildContext context, int index) {
         final habit = habits[index];
-    
-        return DecoratedBox(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(25)),
-          child: Container(
-            height: 150,
-            width: 200,
-            color: Colors.green,
+
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.green
+          ),
+          height: widgetHeight,
+          child: Center(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(habit.title),
-                Text(habit.description),
-                Text(habit.createdAt),
+                Text(
+                  habit.title,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Color.fromARGB(198, 42, 42, 42),
+                  ),
+                ),
+                Text(habit.description, style: TextStyle(fontSize: 18)),
+                Text(habit.createdAt, style: TextStyle(fontSize: 12)),
               ],
             ),
           ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 final lightTheme = ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 211, 211, 211),
-        dividerTheme: DividerThemeData(color: Colors.transparent),
-      );
+  scaffoldBackgroundColor: const Color.fromARGB(255, 211, 211, 211),
+  dividerTheme: DividerThemeData(color: Colors.transparent),
+);
